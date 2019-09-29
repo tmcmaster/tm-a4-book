3cf8589bce44c70cca7d688c479759d5b3ec902d
Tim McMaster <tim@mcmaster.id.au>
2019-09-29 16:53:06 +1000

    checkpoint

--- demo/index.js
+++ demo/index.js
@@ -1,5 +1,6 @@
 import {html, render} from "lit-html";
 import '../src/index.js';
+//import '@wonkytech/tm-page-slider';
 
 render(html`
     <style>
@@ -8,6 +9,16 @@ render(html`
           padding: 0;
           margin: 0;
         } 
+        h3 {
+            color: lightgray;
+            text-align: center;
+            margin-top: 40vh;
+        }
     </style>
-    <tm-a4-book></tm-a4-book>
+    <tm-a4-book>
+        <paper-slide slot="page"><h3>aPage One</h3></paper-slide>
+        <paper-slide slot="page"><h3>aPage Two</h3></paper-slide>
+        <paper-slide slot="page"><h3>aPage Three</h3></paper-slide>
+        <paper-slide slot="page"><h3>aPage Four</h3></paper-slide>
+    </tm-a4-book>
 `, document.querySelector('body'));
--- src/tm-a4-book.js
+++ src/tm-a4-book.js
@@ -1,4 +1,4 @@
-import {html, LitElement} from 'lit-element';
+import {html, LitElement, css} from 'lit-element';
 
 class TmA4Book extends LitElement {
 
@@ -12,8 +12,87 @@ class TmA4Book extends LitElement {
     constructor() {
         super();
         this.heading = 'Hello World!';
+
+        // document.onkeydown = () => {
+        //     console.log('Key Press: ', window.event.keyCode);
+        //     switch (window.event.keyCode) {
+        //         case 37: this._prevPage(); break;
+        //         case 38: this._prevPage(); break;
+        //         case 39: this._nextPage(); break;
+        //         case 40: this._nextPage(); break;
+        //     }
+        // };
+
+        // setTimeout(() => {
+        //     const slot = this.shadowRoot.querySelector('#slot');
+        //     slot.addEventListener('slotchange', e => {
+        //         console.log('light dom children changed!');
+        //     });
+        // }, 1000);
+
     }
 
+    static get styles() {
+        //language=CSS
+        return [css`
+            :host {
+                display: flex;
+                justify-content: center;
+                box-sizing: border-box;
+                background: lightgray;
+                width:100vw;
+                height:100vh;
+                font-size: 2vh;
+            }
+            l2t-paper-slider {
+                display: inline-block;
+                box-sizing: border-box;
+                border: solid grey 2px;
+                background: white;
+            }
+
+            @media only screen and (orientation: portrait) and (max-aspect-ratio: 10/14) {
+                :host {
+                    flex-direction: column;
+                    border: solid red 2px;
+                }
+            }
+
+            @media only screen and (orientation: portrait) and (min-aspect-ratio: 10/14), only screen and (orientation: landscape) {
+                :host {
+                    flex-direction: row;
+                    border: solid blue 2px;
+                }
+            }
+
+            @media only screen and (orientation: portrait) {
+                /*noinspection CssUnusedSymbol*/
+                l2t-paper-slider {
+                    border: solid blue 2px;
+                    width: 100vw;
+                    height: calc(100vw * 1.4);
+                }
+            }
+
+            @media only screen and (orientation: portrait) and (min-aspect-ratio: 10/14) {
+                /*noinspection CssUnusedSymbol*/
+                l2t-paper-slider {
+                    border: solid red 2px;
+                    height: 99vh;
+                    width: calc(100vh / 1.4);
+                }
+            }
+
+            @media only screen and (orientation: landscape) {
+                /*noinspection CssUnusedSymbol*/
+                l2t-paper-slider {
+                    border: solid green 2px;
+                    height: 99vh;
+                    width: calc(100vh / 1.4);
+                }
+            }
+        `];
+    }
     // noinspection JSUnusedGlobalSymbols
     render() {
         return html`
@@ -21,15 +100,34 @@ class TmA4Book extends LitElement {
         :host {
           display: inline-block;
         }
-        h2 {
-            color: gray;
-        }
+        
       </style>
-      <h2>${this.heading}</h2>
+      <tm-page-slider id="aaa" hide-nav>
+        <slot name="page"></slot>
+<!--        <paper-slide><h3>Page One</h3></paper-slide>-->
+<!--        <paper-slide><h3>Page Two</h3></paper-slide>-->
+<!--        <paper-slide><h3>Page Three</h3></paper-slide>-->
+<!--        <paper-slide><h3>Page Four</h3></paper-slide>-->
+      </tm-page-slider>
     `;
     }
 
+    _test(a,b,c) {
+        console.log('Slot Changed:', a,b,c);
+        let pages = this.shadowRoot.getElementById('slot').assignedNodes();
+        console.log('Pages: ', pages);
+        //this.shadowRoot.getElementById('aaa').append(pages[0]);
+    }
 
+    _nextPage() {
+        console.log('Next Page');
+        this.shadowRoot.getElementById('aaa').moveNext();
+    }
+
+    _prevPage() {
+        console.log('Previous Page');
+        this.shadowRoot.getElementById('aaa').movePrev();
+    }
 }
 
 window.customElements.define('tm-a4-book', TmA4Book);
