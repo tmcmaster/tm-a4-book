d5c436cb791d5268f14d4dc1b419fcba3f31d344
Tim McMaster <tim@mcmaster.id.au>
2019-11-24 15:03:06 +1100

    update GitHub Pages site.

--- README.md
+++ README.md
@@ -13,5 +13,12 @@ npm i @wonkytech/tm-a4-book
   import 'tm-a4-book';
 </script>
 
-<tm-a4-book></tm-a4-book>
+<tm-a4-book>
+    <tm-a4-page slot="page">
+        <h2>This is a page</h2>
+    </tm-a4-page>
+    <tm-a4-page slot="page">
+        <h2>This is another page</h2>
+    </tm-a4-page>
+</tm-a4-book>
 ```
--- docs/web_modules/@wonkytech/tm-a4-book.js
+++ docs/web_modules/@wonkytech/tm-a4-book.js
@@ -3751,6 +3751,78 @@ function trackForward(info, e, preventer) {
   }
 }
 
+class TmA4Page extends LitElement {
+  // noinspection JSUnusedGlobalSymbols
+  static get properties() {
+    return {};
+  }
+
+  constructor() {
+    super();
+  }
+
+  static get styles() {
+    // language=CSS
+    return css`
+            :host {
+                display: inline-block;
+                display: inline-block;
+                box-sizing: border-box;
+                background: lightgray;
+                //border: solid blue 2px;
+                /*width: 100%;*/
+                /*height: 100%;*/
+                padding: 0 5px 0 5px;
+            }
+
+            @media only screen and (orientation: portrait) {
+                :host {
+                    padding: 0 1vw 0 1vw;
+                }
+            }
+
+            @media only screen and (orientation: portrait) and (min-aspect-ratio: 10/14) {
+                :host {
+                    padding: 0 calc(1vh/1.4) 0 calc(1vh/1.4);
+                }
+            }
+
+            @media only screen and (orientation: landscape) {
+                :host {
+                    padding: 0 calc(1vh/1.4) 0 calc(1vh/1.4);
+                }
+            }
+            
+            .body {
+                display: inline-block;
+                box-sizing: border-box;
+                /border: solid red 2px;
+                width: 100%;
+                height: 100%;
+                background: white;
+                -webkit-touch-callout: none; /* iOS Safari */
+                -webkit-user-select: none; /* Safari */
+                -khtml-user-select: none; /* Konqueror HTML */
+                -moz-user-select: none; /* Old versions of Firefox */
+                -ms-user-select: none; /* Internet Explorer/Edge */
+                user-select: none; /* Non-prefixed version, currently supported by Chrome, Opera and Firefox */
+            }
+        `;
+  } // noinspection JSUnusedGlobalSymbols
+
+
+  render() {
+    return html`
+            <div class="body">
+                <slot></slot>
+            </div>
+        `;
+  }
+
+}
+
+window.customElements.define('tm-a4-page', TmA4Page);
+
 class TmA4Book extends LitElement {
   // noinspection JSUnusedGlobalSymbols
   static get properties() {
@@ -3854,24 +3926,34 @@ class TmA4Book extends LitElement {
                     width: 100vw;
                     height: calc(100vw * 1.4);
                 }
+
+                /*.slider__slides ::slotted(*) {*/
+                /*    padding:1vw;*/
+                /*}*/
             }
 
             @media only screen and (orientation: portrait) and (min-aspect-ratio: 10/14) {
                 /*noinspection CssUnusedSymbol*/
                 .slider {
                     //border: solid red 2px;
-                    height: 99vh;
+                    height: 100vh;
                     width: calc(100vh / 1.4);
                 }
+                /*.slider__slides ::slotted(*) {*/
+                /*    padding:calc(1vh/1.4);*/
+                /*}*/
             }
 
             @media only screen and (orientation: landscape) {
                 /*noinspection CssUnusedSymbol*/
                 .slider {
                     //border: solid green 2px;
-                    height: 99vh;
+                    height: 100vh;
                     width: calc(100vh / 1.4);
                 }
+                /*.slider__slides ::slotted(*) {*/
+                /*    padding:calc(1vh/1.4);*/
+                /*}*/
             }
 
             /*noinspection ALL*/
@@ -3914,6 +3996,7 @@ class TmA4Book extends LitElement {
                 overflow-x: hidden;
                 display: inline-block;
                 box-sizing: border-box;
+                overflow: hidden;
             //border: solid green 3px;
             }
         `];
@@ -3923,6 +4006,7 @@ class TmA4Book extends LitElement {
   render() {
     return html`
             <div id="container" class="slider" data-pos\$="[[position]]">
+                
               <div id="slides" class="slider__slides mouseup">
                 <slot id="slideSlot" name="page"></slot>
               </div>
